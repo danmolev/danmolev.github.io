edbb7472a105d647bd7a0bba4907a07607ef59cb
Daniel Molev <molevd@gmail.com>
2018-04-23 13:35:20 -0400

    basic functionlity looks good

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ function scl(id){
 
 
 	var element = document.getElementById(id);
-
+	var body =  document.getElementById("body");
 	//element.scrollIntoView();
-	element.scrollIntoView(true);
+	//element.scrollIntoView(true);
 	//element.scrollIntoView({block: "end"});
-	//element.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"});
 
+	//element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+	
+
+	var ele = document.getElementById(id);
+ 	window.scrollTo(ele.offsetLeft,ele.offsetTop-(screen.height/100)*25);
+
+ 	var val = ele.offsetTop-(screen.height/100)*25;
+
+ 	window.scrollTo({
+    top: val,
+    behavior: "smooth"
+	});
 }
