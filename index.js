160cc5f00ded981fa624b3032b544b7c08b8acc3
Daniel Molev <molevd@gmail.com>
2018-06-06 10:35:01 -0400

    added mobile subwebsite

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scl(id){
 
 
 	var element = document.getElementById(id);
-	var body =  document.getElementById("body");
+	//var body =  document.getElementById("body");
 	//element.scrollIntoView();
 	//element.scrollIntoView(true);
 	//element.scrollIntoView({block: "end"});
@@ -11,10 +11,10 @@ function scl(id){
 	//element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 	
 
-	var ele = document.getElementById(id);
- 	window.scrollTo(ele.offsetLeft,ele.offsetTop-(screen.height/100)*25);
+	
+ 	//window.scrollTo(ele.offsetLeft,ele.offsetTop-(screen.height/100)*25);
 
- 	var val = ele.offsetTop-(screen.height/100)*25;
+ 	var val = element.offsetTop-(screen.height/100)*25;
 
  	window.scrollTo({
     top: val,
